8822c4605dfdc3cab12cddb7db38d9d442275d90
tinyb0y <tinyb0y@protonmail.com>
2021-10-05 15:43:17 +0530

    Mr.Robot - tryhackme

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211005.1142';
+const cacheName = 'chirpy-20211005.1541';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
