3ade1db3f8c1f3f88e475749e0775ee451b537a1
tinyb0y <tinyb0y@protonmail.com>
2021-10-05 16:34:41 +0530

    Prev Boxes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211005.1541';
+const cacheName = 'chirpy-20211005.1632';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
