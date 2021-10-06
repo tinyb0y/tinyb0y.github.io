4d8ceb68f43ee01645730bbee0bcf10d4f4e8a79
tinyb0y <tinyb0y@protonmail.com>
2021-10-06 17:11:58 +0530

    Year of the Rabbit - TryHackme

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211005.1632';
+const cacheName = 'chirpy-20211006.1711';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
